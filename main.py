fa51affe8ad11c1147443a47452501754b7eff79
Kruttika Abhyankar <109844844+Kruttikaaa@users.noreply.github.com>
2024-03-27 12:35:24 +0530

    y

--- main.py
+++ main.py
@@ -14,8 +14,12 @@ def save_uploaded_file(uploadedfile):
 col1, col2 = st.columns(2)
 
 with col1: 
-    st.write("Project Details:")
-    st.write("Project Details:")
+    st.write("What is Mental Health?")
+    st.write("Mental health is a state of mental well-being that enables people to cope with the stresses of life, realize their abilities, learn well and work well, and contribute to their community. It is an integral component of health and well-being that underpins our individual and collective abilities to make decisions, build relationships and shape the world we live in. Mental health is a basic human right. And it is crucial to personal, community and socio-economic development.")
+    st.write("What is Clustering?")
+    st.write("When you're trying to learn about something, say music, one approach might be to look for meaningful groups or collections. You might organize music by genre, while your friend might organize music by decade. How you choose to group items helps you to understand more about them as individual pieces of music. You might find that you have a deep affinity for punk rock and further break down the genre into different approaches or music from different locations. On the other hand, your friend might look at music from the 1980's and be able to understand how the music across genres at that time was influenced by the sociopolitical climate. In both cases, you and your friend have learned something interesting about music, even though you took different approaches. In machine learning too, we often group examples as a first step to understand a subject (data set) in a machine learning system. Grouping unlabeled examples is called clustering.")
+    st.write("Why Clustering in Mental Health?")
+
 with col2:
     st.write("This is column 2")
 
