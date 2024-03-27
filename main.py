aeed7aaf36af6e2ffc025df59a51c2b1030ba2e9
Kruttika Abhyankar <109844844+Kruttikaaa@users.noreply.github.com>
2024-03-27 12:22:58 +0530

    y

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 import os
 
+
 st.set_page_config(layout="wide",initial_sidebar_state="collapsed")
-st.header("Main Page")
-st.write("This is the main page. Click the button below to go to the second page.")
+st.header("Mindful Marketing for Mental Wellness")
+st.write("Our project focuses on how mental illnesses affect different regions of the country")
 def save_uploaded_file(uploadedfile):
     with open(os.path.join("./", uploadedfile.name), "wb") as f:
         f.write(uploadedfile.getbuffer())
@@ -13,7 +14,8 @@ def save_uploaded_file(uploadedfile):
 col1, col2 = st.columns(2)
 
 with col1: 
-    st.write("This is column 1")
+    st.write("Project Details:")
+    st.write("Project Details:")
 with col2:
     st.write("This is column 2")
 
@@ -25,5 +27,8 @@ with col2:
         st.success(f"File saved to: {file_path}")
         if st.button("Data Vsiualization"):
             switch_page("page2")
-        
+
+
+
+
 
